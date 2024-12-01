5b1b24df1ec54746a7ab593f44d3623579eaa60f
KristijanKrstevski <kristijankrstevski5@gmail.com>
2024-12-02 00:20:02 +0100

    second

--- lab1/lib/pages/home_screen.dart
+++ lab1/lib/pages/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'product.dart'; // Import the Product screen
+import 'product.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -60,64 +60,40 @@ class ClothingItem {
 
 final List<ClothingItem> clothingItems = [
   ClothingItem(
-  name: 'Класична бела маица',
-  description: 'Удобна памучна маица совршена за секојдневно носење. Се одликува со класичен округол изрез и кратки ракави.',
-  price: 19.99,
-  imageUrl: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-),
-ClothingItem(
-  name: 'Сини тексас фармерки',
-  description: 'Класични сини тексас фармерки со редовен крој. Направени од висококвалитетен тексас со мала еластичност за удобност.',
-  price: 59.99,
-  imageUrl: 'https://images.unsplash.com/photo-1542272454315-4c01d7abdf4a?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-),
-ClothingItem(
-  name: 'Црна кожна јакна',
-  description: 'Стилска кожна јакна со модерен крој. Се одликува со повеќе џебови и премиум детали на патентите.',
-  price: 199.99,
-  imageUrl: 'https://images.unsplash.com/photo-1551028719-00167b16eac5?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-),
-ClothingItem(
-  name: 'Летен фустан со цветен дезен',
-  description: 'Лесен и раздвижен летен фустан со прекрасен цветен дезен. Совршен за топли временски пригоди.',
-  price: 49.99,
-  imageUrl: 'https://images.unsplash.com/photo-1572804013309-59a88b7e92f1?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-),
-ClothingItem(
-  name: 'Сив спортски дуксер',
-  description: 'Удобен и практичен спортски дуксер. Идеален за рекреација или опуштено време дома.',
-  price: 39.99,
-  imageUrl: 'https://images.unsplash.com/photo-1589986460039-fdb6832dfb00?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-),
-ClothingItem(
-  name: 'Поло маица во тегет боја',
-  description: 'Елегантна поло маица со кратки ракави. Изработена од квалитетен памук со едноставен дизајн.',
-  price: 29.99,
-  imageUrl: 'https://images.unsplash.com/photo-1562176160-8d64ab8df866?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-),
-ClothingItem(
-  name: 'Зимска јакна со капуљача',
-  description: 'Топла зимска јакна со капуљача и подлога од крзно. Идеална за студени зимски денови.',
-  price: 149.99,
-  imageUrl: 'https://images.unsplash.com/photo-1541099649105-f69ad21f3246?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-),
-ClothingItem(
-  name: 'Спортски шорцеви',
-  description: 'Лесни и удобни шорцеви, идеални за трчање или тренинг. Направени од дишење материјал.',
-  price: 24.99,
-  imageUrl: 'https://images.unsplash.com/photo-1511355504997-749355c89234?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-),
-ClothingItem(
-  name: 'Сукња со висок струк',
-  description: 'Модерна сукња со висок струк, идеална за официјални или вечерни пригоди.',
-  price: 34.99,
-  imageUrl: 'https://images.unsplash.com/photo-1534938665420-cce3e5c09f27?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-),
-ClothingItem(
-  name: 'Бели патички за тренинг',
-  description: 'Удобни и стилски патички со дишење горен дел. Идеални за спорт или секојдневно носење.',
-  price: 79.99,
-  imageUrl: 'https://images.unsplash.com/photo-1578916171720-59c96f06ce15?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-),
-
+    name: 'Класична бела маица',
+    description: 'Удобна памучна маица совршена за секојдневно носење. Се одликува со класичен округол изрез и кратки ракави.',
+    price: 19.99,
+    imageUrl: 'lib/img/1.jpg',
+  ),
+  ClothingItem(
+    name: 'Сини тексас фармерки',
+    description: 'Класични сини тексас фармерки со редовен крој. Направени од висококвалитетен тексас со мала еластичност за удобност.',
+    price: 59.99,
+    imageUrl: 'lib/img/2jpg.jpg',
+  ),
+  ClothingItem(
+    name: 'Црна кожна јакна',
+    description: 'Стилска кожна јакна со модерен крој. Се одликува со повеќе џебови и премиум детали на патентите.',
+    price: 199.99,
+    imageUrl: 'lib/img/3.jpg',
+  ),
+  ClothingItem(
+    name: 'Летен фустан со цветен дезен',
+    description: 'Лесен и раздвижен летен фустан со прекрасен цветен дезен. Совршен за топли временски пригоди.',
+    price: 49.99,
+    imageUrl: 'lib/img/4.jpg',
+  ),
+  ClothingItem(
+    name: 'Сив спортски дуксер',
+    description: 'Удобен и практичен спортски дуксер. Идеален за рекреација или опуштено време дома.',
+    price: 39.99,
+    imageUrl: 'lib/img/5.jpg',
+  ),
+  ClothingItem(
+    name: 'Поло маица во тегет боја',
+    description: 'Елегантна поло маица со кратки ракави. Изработена од квалитетен памук со едноставен дизајн.',
+    price: 29.99,
+    imageUrl: 'lib/img/6.jpg',
+  ),
 ];
+
